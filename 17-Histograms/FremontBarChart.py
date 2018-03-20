814587a77cf53cbaac41f9472d6ba77853eb1192
TiVentures <mike.brothers@verizon.net>
2018-03-20 12:01:01 -0400

    added Fremont Bridge bar chart

---
+++ 17-Histograms/FremontBarChart.py
@@ -0,0 +1,40 @@
+"""
+This bar chart mimics a histogram as the x-axis
+is a continuous time series, and the y-axis sums
+a frequency that is already part of the dataset
+"""
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import pandas as pd
+
+df = pd.read_csv('..\FremontBridgeBicycles.csv')
+
+# Convert the "Date" text column to a Datetime series:
+df['Date'] = pd.to_datetime(df['Date'])
+
+# Add a column to hold the hour:
+df['Hour']=df['Date'].dt.time
+
+# Let pandas perform the aggregation
+df2 = df.groupby('Hour').sum()
+
+trace1 = go.Bar(
+    x=df2.index,
+    y=df2['Fremont Bridge West Sidewalk'],
+    name="Southbound",
+    width=1
+)
+trace2 = go.Bar(
+    x=df2.index,
+    y=df2['Fremont Bridge East Sidewalk'],
+    name="Northbound",
+    width=1
+)
+data = [trace1, trace2]
+
+layout = go.Layout(
+    title='Fremont Bridge Bicyle Traffic by Hour',
+    barmode='stack'
+)
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig, filename='fremont_bridge.html')
