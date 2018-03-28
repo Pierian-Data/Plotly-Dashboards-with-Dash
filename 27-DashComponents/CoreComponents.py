3412201082f580e24824a6c5e8f2b3fa782e9d1e
TiVentures <mike.brothers@verizon.net>
2018-03-28 14:09:25 -0400

    fix CoreComponents.py

--- 27-DashComponents/CoreComponents.py
+++ 27-DashComponents/CoreComponents.py
@@ -11,7 +11,7 @@ app = dash.Dash()
 app.layout = html.Div([
 
     # DROPDOWN https://dash.plot.ly/dash-core-components/dropdown
-    html.P(
+    html.Label('Dropdown'),
     dcc.Dropdown(
         options=[
             {'label': 'New York City', 'value': 'NYC'},
@@ -19,20 +19,32 @@ app.layout = html.Div([
             {'label': 'San Francisco', 'value': 'SF'}
         ],
         value='MTL'
-    )),
+    ),
+
+    html.Label('Multi-Select Dropdown'),
+    dcc.Dropdown(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': u'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value=['MTL', 'SF'],
+        multi=True
+    ),
 
     # SLIDER https://dash.plot.ly/dash-core-components/slider
+    html.Label('Slider'),
     html.P(
     dcc.Slider(
         min=-5,
         max=10,
         step=0.5,
         marks={i: i for i in range(-5,11)},
-        value=-3,
+        value=-3
     )),
 
     # RADIO ITEMS https://dash.plot.ly/dash-core-components/radioitems
-    html.P(
+    html.Label('Radio Items'),
     dcc.RadioItems(
         options=[
             {'label': 'New York City', 'value': 'NYC'},
@@ -40,8 +52,8 @@ app.layout = html.Div([
             {'label': 'San Francisco', 'value': 'SF'}
         ],
         value='MTL'
-    ))
-])
+    )
+], style={'width': 500})
 
 if __name__ == '__main__':
     app.run_server()
