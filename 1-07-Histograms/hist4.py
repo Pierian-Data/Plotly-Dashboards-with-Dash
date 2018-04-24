1a15502d9599b1d5f9a87410dea7a12af492631a
TiVentures <mike.brothers@verizon.net>
2018-04-24 09:47:57 -0400

    created simpler hist4.py example

--- 1-07-Histograms/hist4.py
+++ 1-07-Histograms/hist4.py
@@ -1,19 +1,26 @@
 #######
-# This histogram displays the number of Reddit button presses
-# over the two months of their social experiment.
+# This histogram compares heights by gender
 ######
 import plotly.offline as pyo
 import plotly.graph_objs as go
 import pandas as pd
 
-df = pd.read_csv('../data/thebutton_presses.csv')
+df = pd.read_csv('../data/arrhythmia.csv')
 
 data = [go.Histogram(
-    x=df['press time']
+    x=df[df['Sex']==0]['Height'],
+    opacity=0.75,
+    name='Male'
+),
+go.Histogram(
+    x=df[df['Sex']==1]['Height'],
+    opacity=0.75,
+    name='Female'
 )]
 
 layout = go.Layout(
-    title="Number of presses per timeslot"
+    barmode='overlay',
+    title="Height comparison by gender"
 )
 fig = go.Figure(data=data, layout=layout)
-pyo.plot(fig, filename='button_presses.html')
+pyo.plot(fig, filename='basic_histogram2.html')
